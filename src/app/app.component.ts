f5a243448c3a7bd06bec4fb63650b1ace33146d4
coolba73 <coolba73@outlook.com>
2017-06-15 16:28:02 +0900

    modified:   src/app/app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,7 @@ export class AppComponent {
     let context : CanvasRenderingContext2D = this.myCanvas.nativeElement.getContext("2d");
     context.fillStyle = 'blue';
     // context.fillRect(10,10,150,150);
+    context.fillRect(10,10,150,150);
   }
 
 }
