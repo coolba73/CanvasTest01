5031b7bd322217c399c167b2c70061ef4a6570fd
coolba73 <coolba73@outlook.com>
2017-06-29 17:14:46 +0900

    line arrow set

--- src/app/shared/shape/LineBase.ts
+++ src/app/shared/shape/LineBase.ts
@@ -16,6 +16,10 @@ export class LineBase extends BaseObject{
     Box_2_PointIndex : number;
 
     LineColor : string = 'black';
+    ArrowFillColor : string = 'lightblue';
+
+    YesDrawStartArrow : boolean;
+    YesDrawEndArrow : boolean
 
 
     //________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________
@@ -32,7 +36,105 @@ export class LineBase extends BaseObject{
         ctx.strokeStyle = this.LineColor;
         ctx.moveTo(this.x1, this.y1);
         ctx.lineTo(this.x2, this.y2);
+        ctx.lineWidth = 1;
         ctx.stroke();
+
+        // this.DrawArror(ctx,'end');
+        this.DrawArrow2(ctx);
+
+    }
+
+    //________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________
+    DrawArrow1(ctx:CanvasRenderingContext2D, where:string)
+    {
+        let from_x : number;
+        let from_y : number;
+        let to_x : number;
+        let to_y : number;
+        let r : number = 10;
+
+        if(where === 'start'){
+
+            from_x = this.x2;
+            from_y = this.y2;
+            to_x = this.x1;
+            to_y = this.y1;
+
+        }
+        else if(where === 'end'){
+
+            from_x = this.x1;
+            from_y = this.y1;
+            to_x = this.x2;
+            to_y = this.y2;
+
+        }
+
+        var x_center = to_x;
+        var y_center = to_y;
+        
+        var angle;
+        var x;
+        var y;
+        
+        ctx.beginPath();
+        
+        angle = Math.atan2(to_y-from_y,to_x-from_x)
+        x = r*Math.cos(angle) + x_center;
+        y = r*Math.sin(angle) + y_center;
+
+        ctx.moveTo(x, y);
+        
+        angle += (1/3)*(2*Math.PI)
+        x = r*Math.cos(angle) + x_center;
+        y = r*Math.sin(angle) + y_center;
+        
+        ctx.lineTo(x, y);
+        
+        angle += (1/3)*(2*Math.PI)
+        x = r*Math.cos(angle) + x_center;
+        y = r*Math.sin(angle) + y_center;
+        
+        ctx.lineTo(x, y);
+        
+        ctx.closePath();
+        ctx.fillStyle = this.LineColor;
+        ctx.fill();
+
+        
+    }
+
+    //________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________
+    DrawArrow2(ctx:CanvasRenderingContext2D)
+    {
+        ctx.save();
+
+        let from_x : number;
+        let from_y : number;
+        let to_x : number;
+        let to_y : number;
+        let size : number = 10;
+
+        from_x = this.x1;
+        from_y = this.y1;
+        to_x = this.x2;
+        to_y = this.y2;
+
+        let dx = to_x - from_x;
+        let dy = to_y - from_y;
+        let len = Math.sqrt(dx*dy+dy*dy);
+        ctx.translate(to_x,to_y);
+        ctx.rotate(Math.atan2(dy,dx));
+
+        ctx.beginPath();
+        ctx.moveTo(0,0);
+        ctx.lineTo(-size, -size);
+        ctx.lineTo(-size, size);
+        ctx.closePath();
+        ctx.fillStyle = this.LineColor;
+        ctx.fill();
+
+        ctx.restore();
 
     }
 
